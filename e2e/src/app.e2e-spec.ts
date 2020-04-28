6d8723aa97b1f5a85e6162045b85bf2989510b16
Ezequiel Marchena <ezmg92@gmail.com>
2020-04-28 21:00:06 +0200

    Añade bootstrap y crea los componentes principales de la aplicacion

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('spotiz app is running!');
+    expect(page.getTitleText()).toEqual('spotyz app is running!');
   });
 
   afterEach(async () => {
